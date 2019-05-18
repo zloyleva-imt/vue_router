526ce69dfd1e9c897ad56f6cd35c7bd6ea7238a8
ZloyLeva <zloyleva@gmail.com>
2019-05-18 11:02:56 +0300

    create About route

--- src/main.js
+++ src/main.js
@@ -5,12 +5,18 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 import Home from './components/Home';
+import About from './components/About';
 
 const routes = [
 	{
 		path: '/',
 		name: 'home',
 		component: Home,
+	},
+	{
+		path: '/about',
+		name: 'about',
+		component: About,
 	}
 ];
 
