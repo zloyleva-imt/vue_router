d8df24a0743d6236a2d0ecfa60217371397f0fee
ZloyLeva <zloyleva@gmail.com>
2019-05-18 11:01:03 +0300

    create Home route

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <Home msg="Welcome to Your Vue.js App"/>
+	<router-view></router-view>
   </div>
 </template>
 
 <script>
-import Home from './components/Home.vue'
 
 export default {
   name: 'app',
-  components: {
-    Home
-  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Router from 'vue-router';
+Vue.use(Router);
+
+import Home from './components/Home';
+
+const routes = [
+	{
+		path: '/',
+		name: 'home',
+		component: Home,
+	}
+];
+
 Vue.config.productionTip = false
 
 new Vue({
+	router:new Router({routes}),
   render: h => h(App),
 }).$mount('#app')
